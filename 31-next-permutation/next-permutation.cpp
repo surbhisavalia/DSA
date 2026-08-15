a2f5bd82340c95d56cba7169b697f5c2381c180e
surbhisavalia <surbhi.savalia146478@marwadiuniversity.ac.in>
2026-08-15 16:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (81.15%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+    void nextPermutation(vector<int>& arr) {
+        int n = arr.size();
+        int pvoit = -1;
+        for(int i = n - 2; i >= 0; i--)
+        {
+            if(arr[i] < arr[i + 1])
+            {
+                pvoit = i;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        if(pvoit == -1)
+        {
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+        for(int j = n - 1; j > pvoit; j--)
+        {
+            if(arr[j] > arr[pvoit])
+            {
+                swap(arr[j], arr[pvoit]);
+                break;
+            }
+        }
+        int i = pvoit + 1;
+        int j = n - 1;
+        while(i < j)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
     }
 };
