5a342772ba186df91ca39467c63f6fff87fa6851
surbhisavalia <surbhi.savalia146478@marwadiuniversity.ac.in>
2026-08-15 17:41:53 +0530

    Time: 3 ms (63.51%) | Memory: 12 MB (62.77%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> answer;
+    vector<int> current;
+
+    void solve(vector<int>& nums, vector<bool>& used) {
+
+        if (current.size() == nums.size()) {
+            answer.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i])
+                continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+            current.push_back(nums[i]);
+            used[i] = true;
+            solve(nums, used);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+        solve(nums, used);
+        return answer;
+    }
+};
