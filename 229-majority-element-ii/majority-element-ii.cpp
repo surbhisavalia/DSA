adb56c8bfbb5a8518fc33d5da506e271e96926dc
surbhisavalia <surbhi.savalia146478@marwadiuniversity.ac.in>
2026-08-16 19:06:18 +0530

    Time: 11 ms (19.07%) | Memory: 27.1 MB (6.50%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        map<int, int> count;
         int n = nums.size();
-        int num1 = 0;
-        int num2 = INT_MIN;
-        int count1 = 0;
-        int count2 = 0;
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i<nums.size();i++){
+            count[nums[i]]++;
+        }
+        vector<int> ans;
+        int use = n/3;
 
-            if (nums[i] == num1) {
-                count1++;
-            }
-            else if (nums[i] == num2) {
-                count2++;
-            }
-            else if (count1 == 0) {
-                num1 = nums[i];
-                count1 = 1;
-            }
-            else if (count2 == 0) {
-                num2 = nums[i];
-                count2 = 1;
-            }
-            else {
-                count1--;
-                count2--;
+        for(auto it = count.begin(); it!=count.end();it++){
+            if(it->second > use){
+                ans.push_back(it->first);
             }
         }
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == num1)
-                count1++;
-            if (nums[i] == num2)
-                count2++;
-        }
-        vector<int> answer;
-        if (count1 > n / 3)
-            answer.push_back(num1);
-        if (num2 != num1 && count2 > n / 3)
-            answer.push_back(num2);
-        return answer;
+        return ans;
     }
 };
