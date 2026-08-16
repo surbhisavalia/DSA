7a9df10a5fb869bd26c03c06352845cadf27adb0
surbhisavalia <surbhi.savalia146478@marwadiuniversity.ac.in>
2026-08-16 19:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (18.51%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        int maxSide = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        dp[i][j] = min(
+                            dp[i - 1][j],
+                            min(dp[i][j - 1], dp[i - 1][j - 1])
+                        ) + 1;
+                    }
+                    maxSide = max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+};
