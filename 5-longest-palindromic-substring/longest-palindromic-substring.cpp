ca133dfc78f4a610f9447537cc3ebeb6e7598426
surbhisavalia <surbhi.savalia146478@marwadiuniversity.ac.in>
2026-08-16 19:17:51 +0530

    Time: 6 ms (88.09%) | Memory: 9.2 MB (91.95%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n <= 1) {
+            return s;
+        }
+        int start = 0;
+        int maxLength = 1;
+        for (int i = 0; i < n; i++) {
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substr(start, maxLength);
+    }
+};
